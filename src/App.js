b8f83e66d4b5ef92ae23e0cfefe394cb0dd7f4f8
Rajesh Mathi <mathirajesh7773@gmail.com>
2024-02-07 01:52:18 -0600

    Rajesh changes pushed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./components/Login";
-import Tutor from "./components/Tutor";
-import Trainee from "./components/Trainee";
+import NavBar from "./components/NavBar";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Login from "./components/Login";
+// import Tutor from "./components/Tutor";
+// import Trainee from "./components/Trainee";
 // import backgroundimage from "./images";
 // import "../App.css";
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes >
-          <Route path="/" element={<Login />} />
-          <Route path="/tutor" element={<Tutor />} />
-          <Route path="/trainee" element={<Trainee />} />
-        </Routes>
-      </Router>
+    //   <div>
+    //     <Router>
+    //       <Routes >
+    //         <Route path="/" element={<Login />} />
+    //         {/*<Route path="/tutor" element={<Tutor />} />
+    // <Route path="/trainee" element={<Trainee />} />*/}
+    //       </Routes>
+    //     </Router>
+    //   </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <NavBar />
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">Body</div>
+      </div>
+
+      <div class="row">
+        <div class="col">Footer</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3 bg-white">Column 1</div>
+        <div className="col-md-3 bg-white">Column 2</div>
+        <div className="col-md-3 bg-white">Column 3</div>
+        <div className="col-md-3 bg-white">Column 4</div>
+      </div>
+    </div>
+  );
+};
+export default NavBar;
